a7d7616bbfb25667b74b946b27215b6d8af0f3ce
Amantay Sultanbek <karinkatochikato@gmail.com>
2023-02-09 15:16:50 +0600

    Initial Commit

---
+++ 1Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1Game
+//
+//  Created by Cashpro on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
